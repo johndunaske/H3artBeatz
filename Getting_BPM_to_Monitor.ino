667f22d0b3624521c39d0200d8d1a7df09948e72
John K Dunaske <johnduna@buffalo.edu>
2021-11-06 17:59:20 -0400

    Changed around GPIO - Tillo

--- Getting_BPM_to_Monitor.ino
+++ Getting_BPM_to_Monitor.ino
@@ -13,8 +13,8 @@
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
 //  Variables
-const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
-const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
+const int PulseWire = A1;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 4;          // The on-board Arduino LED, close to PIN 13.
 int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
                                // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
                                // Otherwise leave the default "550" value. 
