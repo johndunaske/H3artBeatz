e07a9c04495327365eedc333ca985ae3d7ec08b6
John K Dunaske <j_dunaske@yahoo.com>
2021-11-07 10:32:07 -0500

    Maybe work around??

--- heartbeats-frontend/src/Components/LoginPage.jsx
+++ heartbeats-frontend/src/Components/LoginPage.jsx
@@ -5,7 +5,7 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize?';
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "0f76c14b4897491db4970cd235a10161";
 //const redirectUri = "http://localhost:3000";
-const redirectUri = "https://h3artbeatz.herokuapp.com/";
+const redirectUri = "http://h3artbeatz.herokuapp.com/";
 const scopes = [
   "user-read-playback-state",
   "streaming",
